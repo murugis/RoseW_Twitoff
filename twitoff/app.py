11968cb323226a2c9bf5770240a601b6d496e3a4
Rose Wachira <r.kutchee@gmail.com>
2020-10-13 11:30:10 -0500

    Added the final routes to deploy heroku

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
-from.db_model import DB, User,Tweet
+from os import getenv
+from flask import Flask,render_template,request#knows how to read the html file
+from .db_model import DB, User
+from .twitter import add_user_tweepy,update_all_users
+from .predict import predict_user
 
 
 def create_app():
@@ -11,23 +14,59 @@ def create_app():
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        return render_template('base.html',title='Home', users=User.query.all())
 
-    #@app.route('/<username>/<followers>')
-    #def add_user(username,followers):
-        #user = User(username=username, followers=followers)
-        #DB.session.add(user)
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+
+    def add_or_update_user(name=None, message=''):
+        name = name or request.values['user_name']
+
+        try:
+            if request.method == "POST":
+                add_user_tweepy(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username == name).one().tweet
+        except Exception  as e:
+            print(f'Error adding {name}: {e}')
+            tweets = []
+        
+        return render_template('user.html',title=name, tweets=tweets, message=message)
+    
+    @app.route("/compare", methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = "Cannot compare a user to themselves"
+        else:
+            prediction = predict_user(user1,user2,tweet_text)
+
+            message = f'''{tweet_text} is more likely to be said by {user1 if prediction else user2} 
+                          than {user2 if prediction else user1}'''
+
+
+        return render_template('predict.html', title='Prediction', message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset Database!', users=User.query.all())
+    
+    @app.route('/update', methods=['GET'])
+    def update():
+        update_all_users()
+        return render_template('base.html', title='All Tweets Updated',users=User.query.all())
+    
+    #@app.route('/<user_id>/<tweet>')
+    #def add_tweet(user_id,tweet):
+        #tweet = Tweet(user_id=user_id, tweet=tweet)
+        #DB.session.add(tweet)
         #DB.session.commit()
 
-        #return f'{username} has been added to the DB!'
-
-    
-    @app.route('/<user_id>/<tweet>')
-    def add_tweet(user_id,tweet):
-        tweet = Tweet(user_id=user_id, tweet=tweet)
-        DB.session.add(tweet)
-        DB.session.commit()
-
-        return f'{tweet} has been added to the DB!'
+        #return f'{tweet} has been added to the DB!'
 
     return app
--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -2,16 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 class User(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
     followers = DB.Column(DB.String(120), unique=True, nullable=False)
+    # Tweets IdDs ate ordinal ints,so we can fetch most recent data
+    newest_tweet_id = DB.Column(DB.BigInteger,nullable=False)
     def __repr__(self):
         return '<User %r>' % self.username
         
 class Tweet(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     tweet = DB.Column(DB.String(280), unique=True, nullable=False)
+    embedding = DB.Column(DB.PickleType,nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)#connects the two tables
     user = DB.relationship('User', backref=DB.backref('tweet', lazy=True))#create a tweet in the user table
+    
     def __repr__(self):
         return '<Tweet %r>' % self.tweet #dander methods spits the details
